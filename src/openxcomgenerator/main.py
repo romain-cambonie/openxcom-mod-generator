d4a22d1c8f7ecdb2a25e5307a22dd6f0efc291b0
Romain Cambonie <romain.cambonie@gmail.com>
2023-12-17 19:46:21 +0100

    feat: added concept art step before refining prompt for dalle

--- src/openxcomgenerator/main.py
+++ src/openxcomgenerator/main.py
@@ -5,9 +5,13 @@ from typing import Dict
 import sys
 import json
 import openai
+from casefy import kebabcase  # type: ignore[attr-defined]
 
+from chat.ask_for_dalle_character_prompt import ask_for_dalle_character_prompt
 from chat.ask_for_origin_story import ask_for_origin_story
+from chat.ask_for_visual_proposition import ask_for_concept_art
 from chat.image_description import describe_image
+from dalle.call_dalle_and_save_image import call_dalle_and_save_image
 from image.crop import crop_image_to_square
 from image.paint_black_rectangle import paint_black_rectangle
 from modding.translate_yaml import translate
@@ -27,6 +31,12 @@ from utils.dotenv import EnvironmentVariables, get_environment_variables_from_do
 # The only registered command for now is 'generateBackstory'
 
 
+# Write the given string content to a given file path
+def save(content: str, path: Path) -> None:
+    with open(path.as_posix(), "w") as file:
+        file.write(content)
+
+
 def main(json_payload_string: str) -> None:
     # Parse the JSON string into a Python dictionary
     args: Dict[str, str] = json.loads(json_payload_string)
@@ -38,6 +48,7 @@ def main(json_payload_string: str) -> None:
 
     env: EnvironmentVariables = get_environment_variables_from_dotenv()
 
+    # Translate game payload
     # As of now payload contains the path to the payload.yml generated from the character Yaml:Node in the game.
     payload_file: Path = Path(args["payload"])
     language_filename: str = env["LANGUAGE_LOCALE"] + ".yml"
@@ -54,9 +65,14 @@ def main(json_payload_string: str) -> None:
         locale=locale,
     )
 
+    character_identifier = kebabcase(character_name)
+
     with open(payload_localized_output_path, "r") as file:
         character_game_description = file.read()
 
+    print(f"Character equipment translation done. Saved to: {payload_localized_output_path.as_posix()}")
+
+    # Describe screenshot
     game_screenshot: Path = Path(args["screenshot"])
     cropped_image_gpt4_vision: Path = Path(env["WORKING_DIRECTORY"]) / "image-cropped.png"
 
@@ -86,29 +102,61 @@ def main(json_payload_string: str) -> None:
     )
 
     # Ask for image description
-    openAIClient = openai.OpenAI(
+    open_ai_client = openai.OpenAI(
         api_key=env["OPEN_AI_API_KEY"],
     )
-    #
-    #
-    character_image_description = describe_image(client=openAIClient, image_path=cropped_image_gpt4_vision)
 
+    character_image_description = describe_image(client=open_ai_client, image_path=cropped_image_gpt4_vision)
+
+    output_vision: Path = Path(env["WORKING_DIRECTORY"]) / f"{character_identifier}-gpt4-vision-description.txt"
+    save(character_image_description, output_vision)
+    print(f"Character equipment description done, saved to: {output_vision.as_posix()}")
+
+    # Generate Origin Story
     story: str = ask_for_origin_story(
-        client=openAIClient,
+        client=open_ai_client,
         character_name=character_name,
         equipment_description=character_game_description,
         appearance_description=character_image_description,
     )
 
-    print(story)
-    # 1: call chatGPT and ask to create prompt from description
-    # 2: add image to demand (screenshot + cut) with description
+    output_story: Path = Path(env["WORKING_DIRECTORY"]) / f"{character_identifier}-gpt4-vision-origin-story.txt"
+    save(story, output_story)
+    print(f"Character origin story generated, saved to: {output_story.as_posix()}")
+
+    # Generate concept art
+    art_style: str = (
+        "digital concept art style, resembling modern video game illustrations. "
+        "The character should be depicted in ultra-high definition with sharp details, vibrant colors, and clean lines."
+        "Aim for a sleek digital look with smooth textures, and dynamic lighting effects"
+    )
+    concept_art_description: str = ask_for_concept_art(
+        client=open_ai_client,
+        character_story=story,
+        art_style_description=art_style,
+    )
+
+    output_concept_art: Path = Path(env["WORKING_DIRECTORY"]) / f"{character_identifier}-gpt4-concept-art.txt"
+    save(concept_art_description, output_concept_art)
+    print(f"Description of concept art generated, saved to: {output_concept_art.as_posix()}")
+
+    # Refine to Prompt
+    prompt: str = ask_for_dalle_character_prompt(
+        client=open_ai_client,
+        concept_art_description=concept_art_description,
+    )
+
+    output_dalle_prompt: Path = Path(env["WORKING_DIRECTORY"]) / f"{character_identifier}-gpt4-dalle-prompt.txt"
+    save(prompt, output_dalle_prompt)
+    print(f"Character dalle prompt generated, saved to: {output_dalle_prompt.as_posix()}")
+
+    # Generate Image
+    output_path: Path = Path(env["WORKING_DIRECTORY"]) / f"{character_identifier}.png"
+    call_dalle_and_save_image(prompt=prompt, client=open_ai_client, output_file_path=output_path)
+
+    print(f"Character image generated, saved to: {output_path.as_posix()}")
     # 3: prompt engineering following lucille and retro system prompt
     # 3-2: ask for embedding and seed for persistence
-    #
-
-    # translated payload in /home/yandros/Games/OpenXcom/share/openxcom/user/mods/TheCrew/GameData/translated-payload
-    # screenshot
 
 
 #
